052729d7a9c190d8281580dd9f4dd8ed3f381fe7
mattgd <matt.dzwonczyk@live.com>
2015-04-23 11:16:35 -0400

    Initial file commit.

---
+++ LargeClipboard/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mdzwonczyk
+ * Date: 4/21/2015
+ * Time: 11:16 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace LargeClipboard
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.MenuStrip menuStrip;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+		public System.Windows.Forms.RichTextBox clipboard1;
+		public System.Windows.Forms.RichTextBox clipboard2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.clipboard1 = new System.Windows.Forms.RichTextBox();
+			this.clipboard2 = new System.Windows.Forms.RichTextBox();
+			this.menuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip
+			// 
+			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.fileToolStripMenuItem});
+			this.menuStrip.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip.Name = "menuStrip";
+			this.menuStrip.Size = new System.Drawing.Size(284, 24);
+			this.menuStrip.TabIndex = 0;
+			this.menuStrip.Text = "menuStrip";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.aboutToolStripMenuItem,
+			this.helpToolStripMenuItem,
+			this.settingsToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+			this.aboutToolStripMenuItem.Text = "About";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+			this.helpToolStripMenuItem.Text = "Help";
+			// 
+			// settingsToolStripMenuItem
+			// 
+			this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+			this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+			this.settingsToolStripMenuItem.Text = "Settings";
+			// 
+			// clipboard1
+			// 
+			this.clipboard1.Location = new System.Drawing.Point(12, 27);
+			this.clipboard1.Name = "clipboard1";
+			this.clipboard1.Size = new System.Drawing.Size(260, 54);
+			this.clipboard1.TabIndex = 1;
+			this.clipboard1.Text = "";
+			// 
+			// clipboard2
+			// 
+			this.clipboard2.Location = new System.Drawing.Point(12, 87);
+			this.clipboard2.Name = "clipboard2";
+			this.clipboard2.Size = new System.Drawing.Size(260, 54);
+			this.clipboard2.TabIndex = 2;
+			this.clipboard2.Text = "";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.clipboard2);
+			this.Controls.Add(this.clipboard1);
+			this.Controls.Add(this.menuStrip);
+			this.MainMenuStrip = this.menuStrip;
+			this.Name = "MainForm";
+			this.Text = "Large Clipboard";
+			this.menuStrip.ResumeLayout(false);
+			this.menuStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ LargeClipboard/MainForm.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Author: mattgd
+ * Date: 4/21/2015
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LargeClipboard
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form {
+		public MainForm(Boolean open) {
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			if (!open) new ClipboardMonitor();
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		public static void setClipboardText(String text) {
+			if (clipboard1.Text == null) {
+				clipboard1.Text = text;
+			} else {
+				clipboard2.Text = text;
+			}
+		}
+	}
+}
