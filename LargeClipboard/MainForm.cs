eb85a4948a129801faed2256847265a2605d863f
mattgd <matt.dzwonczyk@live.com>
2015-04-29 13:02:14 -0400

    Began working on hotkey code.

--- LargeClipboard/MainForm.cs
+++ LargeClipboard/MainForm.cs
@@ -28,6 +28,7 @@ namespace LargeClipboard
   		                                     IntPtr lParam);
   		
   		IntPtr nextClipboardViewer;
+  		GlobalHotkey ghk;
   		
   		// DLL libraries used to manage hotkeys
 		[DllImport("user32.dll")] 
@@ -36,25 +37,17 @@ namespace LargeClipboard
 		[DllImport("user32.dll")]
 		public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 		
-        public const int ALT = 0x0001;
-        public const int CTRL = 0x0002;
-        public const int WM_HOTKEY_MSG_ID = 0x0312;
-
-		GlobalHotkey ghk;
-  		
+		const int ALT = 0x0001;
+        const int CTRL = 0x0002;
+        const int WM_HOTKEY_MSG_ID = 0x0312;
+		
 		public MainForm(Boolean open) {
 			InitializeComponent();
 			nextClipboardViewer = (IntPtr)SetClipboardViewer((int) this.Handle);
-			//RegisterHotKey(this.Handle, CLIPBOARD1_HOTKEY_ID, ALT_CONTROL, (int) Keys.V);
-			ghk = new Hotkeys.GlobalHotkey(Constants.ALT + Constants.SHIFT, Keys.V, this);
-
+			ghk = new GlobalHotkey(CTRL + ALT, Keys.V, this);
 		}
-  		
-		void HandleHotkey() {
-			MessageBox.Show("Here");
-        }
 		
-  		protected override void WndProc(ref System.Windows.Forms.Message m) {
+  		protected override void WndProc(ref Message m) {
 		  	// defined in winuser.h
 		  	const int WM_DRAWCLIPBOARD = 0x308;
 		  	const int WM_CHANGECBCHAIN = 0x030D;
@@ -72,15 +65,20 @@ namespace LargeClipboard
 		    			SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
 		    		}
 		    		break;
-		 
 		    	default:
 		      		base.WndProc(ref m);
 		    		break;
 		  	}
-
-		  	if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID) HandleHotkey();
+		  	if (m.Msg == WM_HOTKEY_MSG_ID) {
+				HandleHotkey();
+			}  
+            base.WndProc(ref m);
 		}
-  	
+        
+        void HandleHotkey() {
+        	MessageBox.Show("Hotkey pressed!");
+        }
+		
   		void DisplayClipboardData() {
   			try {
   				IDataObject iData = new DataObject();
@@ -98,7 +96,7 @@ namespace LargeClipboard
 	  			} else if (iData.GetDataPresent(DataFormats.Text)) {
 	  				clipboard.Text = (string) iData.GetData(DataFormats.Text);
 	  			} else {
-	  				clipboard.Text = "Invalid clipboard data.";
+	  				clipboard.Text = "";
 	  			}
   			} catch(Exception e) {
   				MessageBox.Show(e.ToString());
